e71b5645ca011df83d50d2808e05289c81860925
Develing1991 <completed0728@gmail.com>
2023-12-30 13:32:15 +0900

    feat: /cicd

--- src/main/java/com/example/cicdproject/cicd/CicdController.java
+++ src/main/java/com/example/cicdproject/cicd/CicdController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class CicdController {
-    @GetMapping("/")
+    @GetMapping("/cicd")
     public String cicdGet(){
         return "hello cicd";
     }
